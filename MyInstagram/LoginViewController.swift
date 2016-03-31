d54cec6bdd490692f3859fffb24e0fe9d407e4f1
Weifan Lin <wlin15@citymail.cuny.edu>
2016-03-31 19:32:14 -0400

    initial commit: Parse setup

---
+++ MyInstagram/LoginViewController.swift
@@ -0,0 +1,69 @@
+//
+//  LoginViewController.swift
+//  MyInstagram
+//
+//  Created by Weifan Lin on 3/31/16.
+//  Copyright © 2016 Weifan Lin. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    @IBAction func onLogin(sender: AnyObject) {
+        PFUser.logInWithUsernameInBackground(usernameField.text!, password: passwordField.text!) { (user: PFUser?, error: NSError?) -> Void in
+            if user != nil {
+                print("login good")
+                self.performSegueWithIdentifier("LoginSegue", sender: nil)
+            } else {
+                print(error?.localizedDescription)
+            }
+        }
+    }
+    @IBAction func onSignUp(sender: AnyObject) {
+        
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        newUser.signUpInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            if success {
+                print("singup good")
+                self.performSegueWithIdentifier("LoginSegue", sender: nil)
+            } else {
+                print(error?.localizedDescription)
+                if error?.code == 202 {
+                    print("user name is taken")
+                }
+            }
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyInstagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyInstagram
+//
+//  Created by Weifan Lin on 3/30/16.
+//  Copyright © 2016 Weifan Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
